10852cfe7693e0e0e1dc3db1c1cbd640d7583ad6
Rudz1 <nobloko80@gmail.com>
2021-12-16 09:47:18 -0300

    active

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,11 @@ aux.addEventListener('click', ()=>{
     darkTheme('contact')
     darkTheme('navbar')
 })
+
+$(document).ready(function() {
+    $("[href]").each(function() {
+        if (this.href == window.location.href) {
+            $(this).addClass("active");
+        }
+    });
+});
